d2f374d85d90ac5c7ff86a1b57367f1dfdae7230
Victor <vigge.jelmlin@hotmail.com>
2017-03-21 18:32:46 +0100

    Base Classes
    Tog bort default kommentarer och la till Archive och KeyMouseReader
    Klass.

---
+++ Grupp 31-SystemUtveckling/Grupp 31-SystemUtveckling/Archive.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp_31_SystemUtveckling
+{
+    static class Archive
+    {
+        public static Dictionary<string, Texture2D> textureDictionary = new Dictionary<string, Texture2D>();
+
+        public static void Initialize(ContentManager Content)
+        {
+            // Order alphabetically
+            // textureDictionary["name"] = Content.Load<Texture2D>("name_in_content_manager");
+            // ex. textureDictionary["tile_stone"] = Content.Load<Texture2D>("Tiles\tile_stone");
+        }
+    }
+}
