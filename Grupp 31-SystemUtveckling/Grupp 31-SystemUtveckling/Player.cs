99df45e5ceb53a66634e9b6ff8cfc29305456ffb
Korgir <Mattias_lindholm41@hotmail.com>
2017-03-22 10:47:51 +0100

    Lagt till Entity och Player

---
+++ Grupp 31-SystemUtveckling/Grupp 31-SystemUtveckling/Player.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp_31_SystemUtveckling
+{
+    class Player : Entity
+    {
+        Game1 game;
+
+        public Player(Texture2D tex, Vector2 pos): base(tex, pos, new Rectangle(0, 0, tex.Width / 4, tex.Height), new Rectangle((int)pos.X, (int)pos.Y, tex.Width / 4, tex.Height))
+        {
+            this.game = game;
+        }
+
+        private void ChangeDirection(Vector2 dir)
+        {
+
+            Vector2 newDestination = pos + dir * 50.0f;
+            //check if we cna move in the desired direction, if not, do nothing
+            //if (!game.getTileATPositoon(newDestination).Wall)
+            //{
+            //    direction = dir;
+            //    destination = newDestination;
+            //    moving = true;
+            //}
+
+        }
+    }
+}
